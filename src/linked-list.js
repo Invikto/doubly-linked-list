f2d6703d6eec81cb273ee8288bbe01785d3a5449
Arthur Kuznetsov <artorius01@gmail.com>
2019-10-06 01:06:32 +0300

    feat: implement 'LinkedList' class

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,27 +1,96 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+  constructor() {
+    this.arr = [];
+  }
 
-    append(data) {}
+  get length() {
+    return this.arr.length;
+  }
 
-    head() {}
+  get _head() {
+    return this.arr[0];
+  }
 
-    tail() {}
+  get _tail() {
+    const last = this.arr.length - 1;
 
-    at(index) {}
+    return this.arr[last];
+  }
 
-    insertAt(index, data) {}
+  append(data) {
+    const last = this.arr.length - 1;
+    const prev = this.arr[last];
+    const node = new Node(data, prev);
 
-    isEmpty() {}
+    this.arr.push(node);
+    if (this.arr[last]) {
+      this.arr[last].next = node;
+    }
 
-    clear() {}
+    return this;
+  }
 
-    deleteAt(index) {}
+  head() {
+    return this.arr[0] ? this.arr[0].data : null;
+  }
 
-    reverse() {}
+  tail() {
+    const last = this.arr.length - 1;
 
-    indexOf(data) {}
+    return this.arr[last] ? this.arr[last].data : null;
+  }
+
+  at(index) {
+    return this.arr[index].data;
+  }
+
+  insertAt(index, data) {
+    const prev = this.arr[index - 1];
+    const next = this.arr[index];
+    const node = new Node(data, prev, next);
+
+    this.arr.splice(index, 0, node);
+    if (this.arr[index - 1]) {
+      this.arr[index - 1].next = this.arr[index];
+    }
+    if (this.arr[index + 1]) {
+      this.arr[index + 1].prev = this.arr[index];
+    }
+
+    return this;
+  }
+
+  isEmpty() {
+    return this.arr.length === 0;
+  }
+
+  clear() {
+    const n = this.arr.length;
+
+    this.arr.splice(0, n);
+
+    return this;
+  }
+
+  deleteAt(index) {
+    this.arr.splice(index, 1);
+
+    return this;
+  }
+
+  reverse() {
+    this.arr.reverse();
+
+    return this;
+  }
+
+  indexOf(data) {
+    const node = this.arr.find(node => node.data === data);
+
+    return this.arr.indexOf(node);
+  }
 }
 
 module.exports = LinkedList;
